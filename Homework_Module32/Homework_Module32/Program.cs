beba54b3f705eab1093631990cd9bcd458e8d8f1
VladPulyak <vlad.pulyak03@mail.ru>
2022-09-20 01:05:47 +0300

    Added Homework 32

--- Homework_Module32/Homework_Module32/Program.cs
+++ Homework_Module32/Homework_Module32/Program.cs
@@ -1,5 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Homework_Module32;
 using System;
-
-Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+Console.WriteLine("================ Task 1 =======================");
+Task1 task1 = new Task1();
+task1.ListsWithUsernames(task1.arrayWithListsOfUsernames);
+Console.WriteLine("================ Task 2 =======================");
+Task2 task2 = new Task2();
+task2.SwapNumbers(task2.numbers);
+Console.WriteLine("================ Task 3 =======================");
+Task3 task3 = new Task3();
+task3.SearchUniqueChars(task3.str);
+Console.WriteLine("================ Task 4 =======================");
+Task4 task4 = new Task4(new int[]
+{
+    1,2, 3, 4, 5, 6, 7,
+}); 
+foreach(var number in task4.ReturnNumbers())
+{
+    Console.WriteLine(number);
+}
---
+++ Homework_Module32/Homework_Module32/Task3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Homework_Module32
+{
+    internal class Task3
+    {
+        public string str = "sdjkfhsdkjfj128sdsdsds391278371289";
+        
+        public void SearchUniqueChars(string str)
+        {
+            var uniqueChars = new Dictionary<char, int>();
+            foreach(var ch in str)
+            {
+                if(!uniqueChars.ContainsKey(ch))
+                {
+                    uniqueChars.Add(ch, 1);
+                }
+                else
+                {
+                    uniqueChars[ch]++;
+                }
+            }
+            Print(uniqueChars);
+        }
+        private void Print(Dictionary<char,int> uniqueChars)
+        {
+            foreach(var pair in uniqueChars)
+            {
+                Console.WriteLine($"Key: {pair.Key}, Value: {pair.Value}");
+            }
+        }
+    }
+}
