88dafa65655b7721dd04e83ce4761c55a2268ee3
Artyom Zhukovsky <Artyom.Zhukovsky@dip.co.uk>
2022-09-20 17:09:33 +0300

    add #32 homework

---
+++ Homework_Module32/Homework_Module32/Classes/YieldClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace Homework_Module32.Classes
+{
+    internal class YieldClass
+    {
+        private List<int> numberList;
+
+        public YieldClass(List<int> numberList)
+        {
+            this.numberList = numberList;
+        }
+
+        public IEnumerable GetEnumerable()
+        {
+            for (var i = numberList.Count - 1; i >= 0; i--)
+            {
+                if (((numberList[i] % 2) != 0) && ((numberList[i] % 3) != 0))
+                {
+                    yield return numberList[i];
+                }
+            }
+        }
+    }
+}
--- Homework_Module32/Homework_Module32/Program.cs
+++ Homework_Module32/Homework_Module32/Program.cs
@@ -1,5 +1,91 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Homework_Module32.Classes;
 
-using System;
+List<int> numberList = new()
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+};
+var yieldClass = new YieldClass(numberList);
 
-Console.WriteLine("Hello, World!");
+foreach (int number in yieldClass.GetEnumerable())
+{
+
+    Console.WriteLine(number);
+}
+
+List<string> namesList = new()
+{
+    "Сеня", "Саня", "Александр", "Антон", "Саня", "Тоха", "Александр", "Сергей"
+};
+LinkedList<int> numberLinkedList = CreateIntLinkedListWithValues(100_000, 0, 100);
+GetListUniqueNames(namesList);
+SwapEvenOdd(numberLinkedList);
+GetSymbolsCountInString("asdasagasfasdasdasdasf");
+void GetListUniqueNames(List<string> list)
+{
+    var uniqueNames = new HashSet<string>(list);
+
+    Console.WriteLine("List of unique names: ");
+
+    foreach (var name in uniqueNames)
+    {
+        Console.WriteLine(name);
+    }
+}
+LinkedList<int> CreateIntLinkedListWithValues(int numberOfValues, int minValue, int maxValue)
+{
+    LinkedList<int> list = new();
+
+    var random = new Random();
+    for (int i = 1; i < numberOfValues; i++)
+    {
+        var randomNumber = random.Next(minValue, maxValue);
+        list.AddLast(randomNumber);
+    }
+
+    return list;
+}
+void SwapEvenOdd(LinkedList<int> list)
+{
+    var currentValue = list.First;
+
+    for (int i = 1; i < list.Count; i++)
+    {
+        var prevValue = currentValue?.Previous?.Value;
+        var nextValue = currentValue?.Next?.Value;
+        bool isNextValueEven = (nextValue != null && (nextValue % 2) == 0);
+        bool isPrevValueOdd = (prevValue != null && (prevValue % 2) != 0);
+
+        if (isNextValueEven && isPrevValueOdd)
+        {
+            int? tempPrev = prevValue;
+            int? tempNext = nextValue;
+
+            nextValue = tempPrev;
+            prevValue = tempNext;
+
+            Console.WriteLine($"current index: {i}, current value: {currentValue?.Value}");
+        }
+
+        currentValue = currentValue?.Next;
+    }
+}
+void GetSymbolsCountInString(string @string)
+{
+    Dictionary<char, int> symbols = new();
+
+    foreach (char currentSymbol in @string)
+    {
+        if (symbols.TryGetValue(currentSymbol, out int count))
+        {   
+            symbols[currentSymbol] += 1;
+        } else
+        {
+            symbols.Add(currentSymbol, 1);
+        }
+    }
+
+    foreach (var item in symbols)
+    {
+        Console.WriteLine($@"symbol '{item.Key}': {item.Value} times");
+    }
+}
