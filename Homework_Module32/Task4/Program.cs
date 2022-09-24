281c37f37b5d1dc2bd9d97a44a7d874c29b9ba27
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-24 10:25:37 +0300

    Task4 DONE

---
+++ Homework_Module32/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task4
+{
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            var list = Enumerable.Range(1, 10).Select(x => random.Next(0, 20)).ToList();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            var myArray = new MyArray(list);
+
+            Console.WriteLine();
+            Console.WriteLine("New Array:");
+            Console.WriteLine();
+
+            foreach (var item in myArray)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
